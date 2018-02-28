47e2f4ab239940676a9c440dc3c42cfa30fb03d6
Anton Räihä <anraiha@cs.helsinki.fi>
2018-02-28 11:40:06 +0200

    Add imports for date-fns and shortid

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { format, parse, addHours } from 'date-fns';
+import { generate } from 'shortid';
 
 const baseURL = process.env.ENDPOINT;
 
